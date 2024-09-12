64db6d767debabae3fa5fa49b96e6c878b5ed788
vkmilkyw <127933262+vkmilkyw@users.noreply.github.com>
2024-09-12 11:51:46 +0800

    Fix app freeze when running NIKKE

--- PlayTools/Controls/PlayInput.swift
+++ PlayTools/Controls/PlayInput.swift
@@ -34,11 +34,11 @@ class PlayInput {
         }
 
         DispatchQueue.main.asyncAfter(deadline: .now() + 5, qos: .utility) {
+            mode.initialize()
             if mode.cursorHidden() || !ActionDispatcher.cursorHideNecessary {
                 return
             }
             Toast.initialize()
         }
-        mode.initialize()
     }
 }
